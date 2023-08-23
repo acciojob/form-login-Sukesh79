63f583384e061bdf34e0ec9499228ad93f262fc5
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-23 20:57:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let firstname = document.getElementById("fname").value;
+	let lastname = document.getElementById("lname").value;
+	alert(`${firstname} ${lastname}`);		
 }
